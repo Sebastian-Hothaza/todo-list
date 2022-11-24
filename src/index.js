eb3570df639c44bb185d12a169ced6f8caa1adeb
Sebastian <sebastianhothaza@gmail.com>
2022-11-23 22:40:54 -0500

    Modal functionality/logic complete

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import { projectFactory, projects } from "./project";
 import { DOM_ListTasks } from "./page"
 
 const createTaskBtn = document.querySelector('#createTask');
+const modalSubmitBtn = document.querySelector('#modalConfirm');
 const myModal = document.querySelector('#modal');
 
 const inbox = projectFactory('inbox');
 
 createTaskBtn.addEventListener('click', () => {
-    // We need some logic to determine if task is to be added to inbox or some project.
-
+    // We need some logic to determine if task is to be added to inbox or some project. TODO Later
     // TEMP: Assume we are adding to inbox
 
     // Pop up modal and get info from user
     myModal.showModal();
+});
 
-
-
-    // Use this info to build a task object, append the task in the appropriate project
-    const newTask = taskItemFactory('Feed the Dog');
+//TODO: For practice, re-write this fn without arrow fn :)
+modalSubmitBtn.addEventListener('click', () => {
+    const taskTitle = document.querySelector('#modalTitle').value;
+    document.querySelector('#modalTitle').value = ''; //TODO: wrap in a clear funtion
+    
+    // Use modal info to build a task object, append the task in the appropriate project
+     const newTask = taskItemFactory(taskTitle);
 
     inbox.appendTask(newTask);
 
@@ -26,7 +30,5 @@ createTaskBtn.addEventListener('click', () => {
 
 
     console.log("projects:  "+projects);
-
-    
-
+    inbox.printTasks();
 });
